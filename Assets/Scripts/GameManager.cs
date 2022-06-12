767ee19bfe09a7875fed977fa696a0a30d72b2aa
cabbaacb <103894445+cabbaacb@users.noreply.github.com>
2022-06-12 18:46:57 +0300

    rotation added
    added rotation to TurnSwitchRoutine, checker if it is working, created empty object GameManager, also added materials for lighting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,8 +19,10 @@ namespace Checkers
 
         private PointerEventData _eventData;
 
-        private Vector3 _cameraStartPosition = new Vector3(3, 6, 9); // 50 180 0
-        private Vector3 _cameraEndPosition = new Vector3(4, 7, -2); // 50 0 0
+        private Vector3 _cameraBlackPosition = new Vector3(3, 6, 9);
+        private Vector3 _cameraBlackRotation = new Vector3(50, 180, 0);
+        private Vector3 _cameraWhitePosition = new Vector3(4, 7, -2);
+        private Vector3 _cameraWhiteRotation = new Vector3(50, 0, 0);
 
 
 
@@ -80,27 +82,31 @@ namespace Checkers
         {
             while (!isVictory)
             {
-                if (currentPlayer != colorType)
-                {
+                if (colorType == ColorType.White)
+                    yield return MoveFromTo(_camera.transform.position, _cameraWhitePosition, _camera.transform.eulerAngles, _cameraWhiteRotation, 3f);
+                else if (colorType == ColorType.Black)
+                    yield return MoveFromTo(_camera.transform.position, _cameraBlackPosition, _camera.transform.eulerAngles, _cameraBlackRotation, 3f);
+               
+                
+                /*
                     colorType = currentPlayer;
 
-                    if (_camera.transform.position == _cameraEndPosition)
+                    if (_camera.transform.position == _cameraWhitePosition)
                     {
-                        var pos = _cameraEndPosition;
-                        _cameraEndPosition = _cameraStartPosition;
-                        _cameraStartPosition = pos;
+                        var pos = _cameraWhitePosition;
+                        _cameraWhitePosition = _cameraBlackPosition;
+                        _cameraBlackPosition = pos;
                     }
 
-                    StartCoroutine (MoveFromTo(_camera.transform.position, _cameraEndPosition, 3f));
-                    //StartCoroutine (Turning)
+                    yield return MoveFromTo(_camera.transform.position, _cameraWhitePosition, 3f);
+                    //StartCoroutine (Turning)*/
 
-                }
             }
-            yield return null;
+            //yield return null;
         }
 
 
-        private IEnumerator MoveFromTo(Vector3 startPosition, Vector3 endPosition, float time)
+        private IEnumerator MoveFromTo(Vector3 startPosition, Vector3 endPosition, Vector3 startRotation, Vector3 endRotation, float time)
         {
             var currentTime = 0f;//текущее время смещения
             while (currentTime < time)//асинхронный цикл, выполняется time секунд
@@ -108,6 +114,7 @@ namespace Checkers
                 //Lerp - в зависимости от времени (в относительных единицах, то есть от 0 до 1
                 //смещает объект от startPosition к endPosition
                 _camera.transform.position = Vector3.Lerp(startPosition, endPosition, 1 - (time - currentTime) / time);
+                _camera.transform.eulerAngles = Vector3.Lerp(startRotation, endRotation, 1 - (time - currentTime) / time);
                 currentTime += Time.deltaTime;//обновление времени, для смещения
                 yield return null;//ожидание следующего кадра
             }
